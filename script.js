e538e85e1a47d842ed833f1b909aadfd3d532895
Saumya <saumyashetty13112007@gmail.com>
2026-09-21 19:32:11 +0530

    Add JavaScript interactivity

---
+++ script.js
@@ -0,0 +1,22 @@
+// let name = "Saumya";
+// let age = 19;
+
+// console.log(name);
+// console.log(age);
+
+// let college = "My College";
+// console.log(college);
+
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+
+function sayHi() {
+    alert("Hi! Thanks for visiting my portfolio 👋");
+}
